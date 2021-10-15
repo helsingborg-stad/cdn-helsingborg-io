b7b3ed06f7519d4c8e5a5d3066eab029b467a1ac
Ehsan Zilaei <zzilaei@gmail.com>
2021-10-15 08:23:18 +0200

    Feat/storage service (#1)
    * changed region to eu-north-1
    
    * added StorageService with table Navigations
    
    * added aws-sdk to package.json
    
    * added aws-sdk to package.json
    
    * created ApiStack with navigations post routes
    
    * created lambda create for handling navigations post requests
    
    * creating and setting up ApiStack

---
+++ src/create.js
@@ -0,0 +1,31 @@
+import AWS from "aws-sdk"
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+export async function main(event) {
+  // Request body is passed as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      // The attributes of the item to be created
+      id: data.id,
+      description: data.description,
+    },
+  };
+
+  try {
+    await dynamoDb.put(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message }),
+    };
+  }
+}
