62a635261c917cb7943510b685b49a586e294ebc
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-10-18 11:27:45 +0200

    Feat/languages service (#4)
    * added languages service
    
    * fixed language term id param
    
    * added scan method
    
    * added get navigation lamdba
    
    * renamed get guidegorups route
    
    * added items param to create navigation
    
    * get languages route
    
    Co-authored-by: Ehsan Zilaei <zzilaei@gmail.com>

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -7,7 +7,7 @@ export default class ApiStack extends sst.Stack {
   constructor (scope, id, props) {
     super(scope, id, props)
 
-    const { navigationsTable, guidegroupsTable } = props;
+    const { navigationsTable, guidegroupsTable, languagesTable } = props;
 
     // Create the API
     this.api = new sst.ApiGatewayV1Api(this, "Api", {
@@ -15,10 +15,18 @@ export default class ApiStack extends sst.Stack {
         environment: {
           NAVIGATIONS_TABLE_NAME: navigationsTable.tableName,
           GUIDEGROUPS_TABLE_NAME: guidegroupsTable.tableName,
+          LANGUAGES_TABLE_NAME: languagesTable.tableName,
         },
       },
       routes: {
-        // navigations
+        // Navigations
+        "GET /navigations": {
+          function: {
+            srcPath: "src/navigations/",
+            handler: "getNavigations.main",
+            environment: { tableName: navigationsTable.tableName },
+          },
+        },
         "POST /navigations": {
           function: {
             srcPath: "src/navigations/",
@@ -30,7 +38,7 @@ export default class ApiStack extends sst.Stack {
         "GET /guidegroups": {
           function: {
             srcPath: "src/guidegroups/",
-            handler: "getGuidegroupList.main",
+            handler: "getGuidegroups.main",
             environment: { tableName: guidegroupsTable.tableName },
           },
         },
@@ -41,11 +49,26 @@ export default class ApiStack extends sst.Stack {
             environment: { tableName: guidegroupsTable.tableName },
           },
         },
+        // Languages
+        "GET /languages": {
+          function: {
+            srcPath: "src/languages",
+            handler: "getLanguages.main",
+            environment: { tableName: languagesTable.tableName },
+          }
+        },
+        "POST /languages": {
+          function: {
+            srcPath: "src/languages",
+            handler: "createLanguages.main",
+            environment: { tableName: languagesTable.tableName },
+          }
+        }
       }
     });
 
     // Allow the API to access the table
-    this.api.attachPermissions([navigationsTable, guidegroupsTable])
+    this.api.attachPermissions([navigationsTable, guidegroupsTable, languagesTable])
 
     // Show the API endpoint in the output
     this.addOutputs({
