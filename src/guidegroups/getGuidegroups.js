f7daae3eb7e57f2209be68cbf5e9760439348ae4
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-10-28 15:09:53 +0200

    filter by groupid, lang and include

--- src/guidegroups/getGuidegroups.js
+++ src/guidegroups/getGuidegroups.js
@@ -1,12 +1,29 @@
 import handler from '../util/handler';
 import dynamoDb from '../util/dynamodb';
 
-export const main = handler(async () => {
+export const main = handler(async event => {
+  const { groupId, lang, include } = event.queryStringParameters ?? {};
+
   const params = {
     TableName: process.env.GUIDEGROUPS_TABLE_NAME,
+    KeyConditionExpression: 'groupId = :groupId',
+    FilterExpression: '#lang = :lang',
+    ExpressionAttributeNames: {
+      '#lang': 'lang',
+    },
+    ExpressionAttributeValues: {
+      ':groupId': parseInt(groupId, 10),
+      ':lang': lang,
+    },
   };
 
-  const result = await dynamoDb.scan(params);
+  const result = await dynamoDb.query(params);
+  let { Items: items } = result;
 
-  return result.Items;
+  if (include) {
+    const includedIds = include.split(',').map(id => parseInt(id, 10));
+    items = items.filter(item => includedIds.includes(item.id));
+  }
+
+  return items;
 });
