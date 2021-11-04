756ca2a76c0302229eb0228908071063d2975343
Ehsan Zilaei <zzilaei@gmail.com>
2021-11-04 16:03:11 +0100

    feat(get-navigation): added handling for query parameters city and language

--- src/navigations/getNavigations.js
+++ src/navigations/getNavigations.js
@@ -1,12 +1,51 @@
 import handler from '../util/handler';
 import dynamoDb from '../util/dynamodb';
 
-export const main = handler(async () => {
-  const params = {
-    TableName: process.env.NAVIGATIONS_TABLE_NAME,
+export const main = handler(async event => {
+  const { userGroupID: queryCity, lang: queryLanguage } = event.queryStringParameters ?? {};
+
+  let params = { TableName: process.env.NAVIGATIONS_TABLE_NAME };
+  let KeyConditionExpression = '';
+  const ExpressionAttributeValues = {};
+  const ExpressionAttributeNames = {};
+
+  // Return whole table if no query parameters found.
+  if (typeof queryCity == 'undefined' && typeof queryLanguage == 'undefined') {
+    const result = await dynamoDb.scan(params);
+
+    return result.Items;
+  }
+
+  // Filter by Global Secondary Index guideLanguage if only language queried.
+  if (typeof queryCity == 'undefined' && typeof queryLanguage != 'undefined') {
+    params['IndexName'] = 'guideLanguage';
+    KeyConditionExpression += '#language = :queryLanguage';
+    ExpressionAttributeValues[':queryLanguage'] = queryLanguage;
+    ExpressionAttributeNames['#language'] = 'language';
+  }
+
+  // DynamoDB parameters if only city queried.
+  if (typeof queryCity != 'undefined') {
+    KeyConditionExpression += '#city = :queryCity ';
+    ExpressionAttributeValues[':queryCity'] = parseInt(queryCity);
+    ExpressionAttributeNames['#city'] = 'city';
+  }
+
+  // DynamoDB language parameters if city AND language queried.
+  if (typeof queryCity != 'undefined' && typeof queryLanguage != 'undefined') {
+    KeyConditionExpression += ' AND begins_with(#language_recordUid, :queryLanguage)';
+    ExpressionAttributeValues[':queryLanguage'] = queryLanguage;
+    ExpressionAttributeNames['#language_recordUid'] = 'language_recordUid';
+  }
+
+  params = {
+    ...params,
+    KeyConditionExpression,
+    ExpressionAttributeValues,
+    ExpressionAttributeNames,
   };
 
-  const result = await dynamoDb.scan(params);
+  const result = await dynamoDb.query(params);
 
   return result.Items;
 });
