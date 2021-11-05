0b80bcb67f1da8b34ca5bfd39ba0b0aa8311ddf0
Ehsan Zilaei <zzilaei@gmail.com>
2021-11-05 14:18:07 +0100

    feat(delete-navigation): lambda for deleting a navigation field

---
+++ src/navigations/deleteNavigation.js
@@ -0,0 +1,24 @@
+import handler from '../util/handler';
+import dynamoDb from '../util/dynamodb';
+
+export const main = handler(async event => {
+  const { city: pathParamCity } = event.pathParameters;
+  const { language: pathParamLanguage } = event.pathParameters;
+  const { id: pathParamId } = event.pathParameters;
+
+  console.log(`city ${pathParamCity}, lang ${pathParamLanguage}, id ${pathParamId}`);
+
+  const params = {
+    TableName: process.env.NAVIGATIONS_TABLE_NAME,
+    Key: {
+      city: parseInt(pathParamCity, 10),
+      language_recordUid: `${pathParamLanguage}#${pathParamId}`,
+    },
+  };
+
+  console.log('delete params', params);
+
+  await dynamoDb.delete(params);
+
+  return { body: { status: true } };
+});
