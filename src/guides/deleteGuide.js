ca58915267b07b67ab0f1e142740f63ef3fa59f2
Ehsan Zilaei <zzilaei@gmail.com>
2021-11-19 14:07:45 +0100

    feat(delete-guides): added delete guides

---
+++ src/guides/deleteGuide.js
@@ -0,0 +1,17 @@
+import handler from '../util/handler';
+import dynamoDb from '../util/dynamodb';
+
+export const main = handler(async event => {
+  const { id: pathParamId } = event.pathParameters;
+
+  const params = {
+    TableName: process.env.GUIDES_TABLE_NAME,
+    Key: {
+      id: parseInt(pathParamId, 10),
+    },
+  };
+
+  await dynamoDb.delete(params);
+
+  return { body: { status: true } };
+});
