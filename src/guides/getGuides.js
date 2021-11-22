19e5165946dc3ffad76aae5d15fb0e975c058c5b
Ehsan Zilaei <zzilaei@gmail.com>
2021-11-22 14:47:54 +0100

    feat(get-guides): handle query guide group id

--- src/guides/getGuides.js
+++ src/guides/getGuides.js
@@ -2,27 +2,44 @@ import handler from '../util/handler';
 import dynamoDb from '../util/dynamodb';
 
 export const main = handler(async event => {
-  const { include: queryInclude } = event.queryStringParameters ?? {};
+  const { include: queryInclude, guideGroupId: queryGuideGroupId } =
+    event.queryStringParameters ?? {};
 
-  const parameterKeys = [];
+  if (typeof queryGuideGroupId !== 'undefined' && typeof queryInclude == 'undefined') {
+    const params = { TableName: process.env.GUIDES_TABLE_NAME };
 
-  queryInclude.split(',').map(includeIds => {
-    parameterKeys.push({
-      id: parseInt(includeIds, 10),
+    const allGuides = await dynamoDb.scan(params);
+
+    if (allGuides.Items.length < 1) {
+      return [];
+    }
+
+    return allGuides.Items.filter(
+      item => item.guidegroup[0]['id'] === parseInt(queryGuideGroupId, 10)
+    );
+  }
+
+  if (typeof queryInclude !== 'undefined' && typeof queryGuideGroupId === 'undefined') {
+    const parameterKeys = [];
+
+    queryInclude?.split(',').map(includeIds => {
+      parameterKeys.push({
+        id: parseInt(includeIds, 10),
+      });
     });
-  });
 
-  const params = {
-    RequestItems: {
-      [process.env.GUIDES_TABLE_NAME]: {
-        Keys: parameterKeys,
+    const params = {
+      RequestItems: {
+        [process.env.GUIDES_TABLE_NAME]: {
+          Keys: parameterKeys,
+        },
       },
-    },
-  };
+    };
 
-  const result = await dynamoDb.batchGet(params);
+    const result = await dynamoDb.batchGet(params);
 
-  // TODO: Handling for unprocessed keys.
+    return result.Responses[process.env.GUIDES_TABLE_NAME];
+  }
 
-  return result.Responses[process.env.GUIDES_TABLE_NAME];
+  return [];
 });
