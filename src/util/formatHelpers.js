d8b1a7412159c9b5ac261c0c028a627cee6f796d
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-11-23 11:40:53 +0100

    parse guides (#14)

--- src/util/formatHelpers.js
+++ src/util/formatHelpers.js
@@ -162,3 +162,212 @@ export const parseGuideGroup = item => {
 
   return guideGroup;
 };
+
+const parseGuideType = item => {
+  const acceptedGuideTypes = ['guide', 'trail'];
+  return acceptedGuideTypes.find(type => type === item);
+};
+
+const getPostStatus = active => (active ? 'publish' : 'draft');
+
+function parseImageUrls(data) {
+  const images = [];
+  try {
+    if (data instanceof Array) {
+      for (const item of data) {
+        const image = parseImages(item.sizes);
+        images.push(image);
+      }
+    }
+  } catch (error) {
+    // something went wrong
+    console.error('Failed to parse images data: ', data);
+  }
+  return images;
+}
+
+function parseDate(data) {
+  if (data === null || data === undefined) {
+    throw new Error('Can not parse null/undefined to a date.');
+  }
+  return new Date(data).toISOString();
+}
+
+function parseMediaContent(data) {
+  if (!(data instanceof Object)) {
+    throw new Error('Failed to parse media content from data: ' + data);
+  }
+
+  const media = {
+    contentType: data.type,
+    created: parseDate(data.date),
+    description: String(data.description),
+    id: Number(data.id),
+    modified: parseDate(data.modified),
+    title: String(data.title),
+    url: new URL(data.url).toString(),
+  };
+  return media;
+}
+
+const parseSubAttraction = (id, subAttractions, locations) => {
+  const subAttractionData = subAttractions.find(item => {
+    const { content } = item;
+    return content instanceof Array && content.indexOf(id) > -1;
+  });
+
+  // Parse location data
+  let location;
+  try {
+    const { location: locationId } = subAttractionData;
+    const locationData = locations.find(locData => locData.id === locationId);
+    location = parseLocation(locationData);
+  } catch (error) {
+    // discarding faulty location data
+  }
+
+  return location;
+};
+
+const parseLink = data => ({
+  title: data.title,
+  type: data.service,
+  url: new URL(data.link).toString(),
+});
+
+const parseLinks = data => {
+  const links = [];
+  for (const item of data) {
+    try {
+      if (item) {
+        const link = parseLink(item);
+        links.push(link);
+      }
+    } catch (error) {
+      // discarding link
+    }
+  }
+  return links;
+};
+
+const parseContentObject = (key, data, subAttractions, locations) => {
+  if (typeof data.order !== 'number') {
+    console.log('parseContentObject error. bail');
+    throw new Error('Failed to parse order from ' + data);
+  }
+
+  const postStatus = getPostStatus(data.active);
+  const images = parseImageUrls(data.image);
+
+  const obj = {
+    id: key,
+    images,
+    order: Number(data.order),
+    postStatus,
+    searchableId: data.id,
+    title: data.title,
+  };
+
+  if (data.description_plain) {
+    obj.description = data.description_plain;
+  }
+
+  if (data.audio) {
+    obj.audio = parseMediaContent(data.audio);
+  }
+
+  if (data.video) {
+    obj.video = parseMediaContent(data.video);
+  }
+
+  if (data.links && data.links instanceof Array) {
+    obj.links = parseLinks(data.links);
+  }
+
+  try {
+    const parsedSubAttraction = parseSubAttraction(obj.id, subAttractions, locations);
+    console.log('parsedSubAttraction', parsedSubAttraction);
+    obj.location = parsedSubAttraction;
+  } catch (error) {
+    // ignoring subAttractions from this guide
+  }
+
+  return obj;
+};
+
+const parseContentObjects = (contentData, subAttractionsData, locationsData) => {
+  const keys = Object.keys(contentData);
+
+  let subAttractions = [];
+  if (subAttractionsData instanceof Array) {
+    subAttractions = subAttractionsData;
+  }
+  let locations = [];
+  if (locationsData instanceof Array) {
+    locations = locationsData;
+  }
+
+  const result = [];
+  for (const key of keys) {
+    try {
+      const obj = parseContentObject(key, contentData[key], subAttractions, locations);
+      result.push(obj);
+    } catch (error) {
+      console.error('Failed to parse content object, discarding.');
+    }
+  }
+
+  return result;
+};
+
+export const parseGuide = item => {
+  const guide = {
+    childFriendly: Boolean(item.guide_kids),
+    contentObjects: [],
+    description: item.content.plain_text,
+    guideGroupId: Number(item.guidegroup[0].id),
+    guideType: parseGuideType(item.content_type),
+    id: Number(item.id),
+    images: parseImages(item.guide_images[0].sizes),
+    name: item.title.plain_text,
+    postStatus: item.status,
+    slug: item.slug,
+    tagline: item.guide_tagline,
+  };
+
+  const { contentObjects, subAttractions, _embedded: embeddedData } = item;
+  let locationData = null;
+  if (embeddedData) {
+    locationData = embeddedData.location;
+  }
+
+  // parse guide location
+  const { guide_location: locationId } = item;
+  if (locationId && locationData && locationData instanceof Array) {
+    const foundLoc = locationData.find(loc => loc.id === locationId);
+    if (foundLoc) {
+      try {
+        guide.location = parseLocation(foundLoc);
+      } catch (e) {
+        // ignoring location from this guide
+      }
+    }
+  }
+
+  // content objects
+  if (contentObjects && contentObjects instanceof Object) {
+    guide.contentObjects = parseContentObjects(contentObjects, subAttractions, locationData);
+  }
+
+  if (item.guide_date_start) {
+    const dateStart = parseDate(item.guide_date_start);
+    guide.dateStart = dateStart;
+  }
+
+  if (item.guide_date_end) {
+    const dateEnd = parseDate(item.guide_date_end);
+    guide.dateEnd = dateEnd;
+  }
+
+  return guide;
+};
