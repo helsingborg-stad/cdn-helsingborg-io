5e1e0ed4417fe552e5809e3bac87ca8285a79148
Ehsan Zilaei <zzilaei@gmail.com>
2021-11-23 14:29:57 +0100

    feat(create-guides): global index guideGroupId to table

--- src/guides/createGuides.js
+++ src/guides/createGuides.js
@@ -42,6 +42,9 @@ export const main = handler(async event => {
     },
   };
 
+  // Add global index guideGroupId.
+  params.Item['dynamoDbGlobalIndexGuideGroupId'] = data.guidegroup[0].id.toString(10);
+
   await dynamoDb.put(params);
 
   return params.Item;
