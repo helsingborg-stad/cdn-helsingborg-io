c6e8db6212089294ee68e6feeb31c8c6bed4495c
Ehsan Zilaei <zzilaei@gmail.com>
2021-11-26 10:51:02 +0100

    fix(get-interactive-guides): batch request interactive guides

--- src/interactiveGuides/getInteractiveGuides.js
+++ src/interactiveGuides/getInteractiveGuides.js
@@ -2,23 +2,33 @@ import handler from '../util/handler';
 import dynamoDb from '../util/dynamodb';
 
 export const main = handler(async event => {
-  // const { id: queryId } = event.queryStringParameters ?? {};
-  //
-  // const params = {
-  //   TableName: process.env.INTERACTIVE_GUIDES_TABLE_NAME,
-  //   KeyConditionExpression: '#id = :queryId',
-  //   ExpressionAttributeValues: { ':queryId': queryId },
-  //   ExpressionAttributeNames: { '#id': 'id' },
-  // };
-  //
-  // const result = await dynamoDb.query(params);
-  // return result.Items;
+  const { include: queryInclude } = event.queryStringParameters ?? {};
+
+  if (typeof queryInclude === 'undefined') {
+    return [];
+  }
+
+  const parameterKeys = [];
+
+  queryInclude.split(',').map(includeIds => {
+    parameterKeys.push({
+      id: parseInt(includeIds, 10),
+    });
+  });
+
+  if (parameterKeys.length === 0) {
+    return [];
+  }
 
   const params = {
-    TableName: process.env.INTERACTIVE_GUIDES_TABLE_NAME,
+    RequestItems: {
+      [process.env.INTERACTIVE_GUIDES_TABLE_NAME]: {
+        Keys: parameterKeys,
+      },
+    },
   };
 
-  const result = await dynamoDb.scan(params);
+  const result = await dynamoDb.batchGet(params);
 
-  return result.Items;
+  return result.Responses[process.env.INTERACTIVE_GUIDES_TABLE_NAME];
 });
