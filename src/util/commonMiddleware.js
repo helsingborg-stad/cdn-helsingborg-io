5c36228e6a9d42a4f9c127b86558c393009b368c
Ehsan Zilaei <zzilaei@gmail.com>
2021-12-03 16:05:03 +0100

    feat(common-middleware): added validation for path

--- src/util/commonMiddleware.js
+++ src/util/commonMiddleware.js
@@ -3,12 +3,24 @@ import jsonBodyParser from '@middy/http-json-body-parser';
 import validator from '@middy/validator';
 import httpErrorHandler from '@middy/http-error-handler';
 import Ajv from 'ajv';
+import httpUrlEncodePathParser from '@middy/http-urlencode-path-parser';
 
 const ajv = new Ajv();
 
 export function parseAndValidateJsonBody(lambdaMain, validationSchema) {
   return middy(lambdaMain)
     .use(jsonBodyParser())
+    .use(
+      validator({
+        inputSchema: ajv.compile(validationSchema),
+      })
+    )
+    .use(httpErrorHandler());
+}
+
+export function parseEncodeAndValidatePath(lambdaMain, validationSchema) {
+  return middy(lambdaMain)
+    .use(httpUrlEncodePathParser())
     .use(
       validator({
         inputSchema: ajv.compile(validationSchema),
