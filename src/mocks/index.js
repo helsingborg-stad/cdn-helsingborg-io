270179947843860d6e96c675e154a65a782ea31b
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-12-13 09:37:15 +0100

    added mock files

---
+++ src/mocks/index.js
@@ -0,0 +1,5 @@
+import guideInput from './guide-input';
+import guideResponse from './guide-response';
+import locationInput from './location-input';
+
+export { guideInput, guideResponse, locationInput };
